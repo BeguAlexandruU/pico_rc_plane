9aa13475c391b60b65e59745f6d45b0d8a7e17e9
BeguAlexandruU <alexandru.begu@student.tuiasi.ro>
2026-03-11 21:38:41 +0200

    plane: pid tunning

--- plane/pid_controller.py
+++ plane/pid_controller.py
@@ -43,21 +43,21 @@ class PID:
         return output
 
 
-ROLL_SLOPE = (30 - (-30)) / (127 - (-127))
+ROLL_SLOPE = (90 - (-90)) / (127 - (-127))
 roll_pid = None
 pitch_pid = None
 
 def setup():
     global roll_pid, pitch_pid
-    roll_pid = PID(kp=1.2, ki=0.1, kd=0.05, output_limits=(-127, 127))
-    pitch_pid = PID(kp=1.2, ki=0.1, kd=0.05, output_limits=(-127, 127))
+    roll_pid = PID(kp=1.2, ki=0, kd=0.05, output_limits=(-127, 127))
+    pitch_pid = PID(kp=1.2, ki=0, kd=0.05, output_limits=(-127, 127))
 
 def map_input_to_angle(x):
     # Simplified Formula: (x - in_min) * slope + out_min
-    return (x + 127) * ROLL_SLOPE - 30
+    return (x + 127) * ROLL_SLOPE - 90
 
 def update():
-    # 1. Convert NRF Stick (0-255) to Target Angle (-30 to +30 degrees)
+    # 1. Convert NRF Stick (0-255) to Target Angle (-90 to +90 degrees)
     target_roll = map_input_to_angle(nrf_module.ch3)
     target_pitch = map_input_to_angle(nrf_module.ch4)
     
@@ -67,12 +67,13 @@ def update():
     
     # 3. Compute PID correction
     # The PID returns a value in the -127 to 127 range for your servo_control
-    aileron_output = roll_pid.compute(target_roll, current_roll)
+    aileron_output = roll_pid.compute(target_roll, -current_roll)
     elevator_output = pitch_pid.compute(target_pitch, current_pitch)
     
     servo_control.set_aileron(aileron_output)
     servo_control.set_elevator(elevator_output)
 
     motor_control.set_throttle(nrf_module.ch2)
+
 
 
